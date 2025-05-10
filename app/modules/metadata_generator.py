74c079ebe20b8266576ee246bb40cf0979465a4b
Boss <nbojanovic.dev@gmail.com>
2025-05-11 00:43:00 +0200

    Silence yt-dlp spam

--- app/modules/metadata_generator.py
+++ app/modules/metadata_generator.py
@@ -66,7 +66,7 @@ class MetadataGenerator:
         ]
 
         response = self.client.chat.completions.create(
-            model="gpt-4-turbo",
+            model="gpt-3.5-turbo",
             messages=messages,
             temperature=0.7,
             max_tokens=1600
@@ -101,7 +101,7 @@ class MetadataGenerator:
         ]
 
         response = self.client.chat.completions.create(
-            model="gpt-4-turbo",
+            model="gpt-3.5-turbo",
             messages=messages,
             temperature=0.7,
             max_tokens=1600

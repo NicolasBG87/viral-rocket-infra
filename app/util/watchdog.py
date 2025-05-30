837956f22758eca402e8daa5ae35f173dc80bb7e
Boss <nbojanovic.dev@gmail.com>
2025-05-30 23:45:40 +0200

    Add sleep

--- app/util/watchdog.py
+++ app/util/watchdog.py
@@ -4,7 +4,7 @@ import time
 from app.util.send_runpod_webhook import send_runpod_webhook
 
 
-def watchdog(timeout=1800, job_id):  # e.g. 30 mins
+def watchdog(timeout, job_id):
     time.sleep(timeout)
     send_runpod_webhook(
         job_id,
